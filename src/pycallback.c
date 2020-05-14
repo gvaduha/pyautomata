a3cbe9d767c304d97aedfc121cde42dd25fe1525
gvaduha <gvaduha@gmail.com>
2020-05-14 16:14:07 +0700

    Python extension added

---
+++ src/pycallback.c
@@ -0,0 +1,30 @@
+#include <Python.h>
+
+static PyObject *my_callback = NULL;
+
+static PyObject *
+my_set_callback(dummy, args)
+    PyObject *dummy, *args;
+{
+    PyObject *result = NULL;
+    PyObject *temp;
+
+    if (PyArg_ParseTuple(args, "O:set_callback", &temp)) {
+        if (!PyCallable_Check(temp)) {
+            PyErr_SetString(PyExc_TypeError, "parameter must be callable");
+            return NULL;
+        }
+        Py_XINCREF(temp);         /* Add a reference to new callback */
+        Py_XDECREF(my_callback);  /* Dispose of previous callback */
+        my_callback = temp;       /* Remember new callback */
+        /* Boilerplate to return "None" */
+        Py_INCREF(Py_None);
+        result = Py_None;
+    }
+    return result;
+}
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
